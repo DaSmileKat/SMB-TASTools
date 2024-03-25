bd7193ec71abbed192a77d291c09418cce38e5ef
DaSmileKat <56834300+DaSmileKat@users.noreply.github.com>
2024-03-25 12:42:12 +0800

    Update shellkick.cpp

--- shellkick.cpp
+++ shellkick.cpp
@@ -4,22 +4,22 @@
 using namespace std;
 
 const int TOTAL_KICKS = 10; // The total number of shell kicks required
-const int KICK_TO_STOMP = 6; // Frames between kicking the shell and stomping the shell
-const int FIRST_KICK_FRAME = 3; // The first frame after falling/jumping that you can reach the shell horizontally
+vector<int> KICK_TO_STOMP = { 6, 6, 6, 6, 6, 6, 6, 6, 6, 6 }; // Frames between kicking the shell and stomping the shell
+const int FIRST_KICK_FRAME = 0; // The first frame after falling/jumping that you can reach the shell horizontally
 
 const int GRAVITY = 0x90; // Falling gravity
 const bool DO_JUMP = false; // Whether you jump at the start instead of falling
-const int JUMP_GRAVITY = 0x20; // Jumping gravity (can ignore if DO_JUMP is set to false)
-const int JUMP_INIT = -0x400; // Initial Y speed when jumping (can ignore if DO_JUMP is set to false)
+const int JUMP_GRAVITY = 0x28; // Jumping gravity (can ignore if DO_JUMP is set to false)
+const int JUMP_INIT = -0x500; // Initial Y speed when jumping (can ignore if DO_JUMP is set to false)
 const int MAX_JUMP_FRAMES = 10; // Maximum jump frames that may be required (can ignore if DO_JUMP is set to false)
 
 // Y positions are represented as pixel * 0x100 + subpixel
-const int Y_MIN = 0x5600;
-const int Y_MAX = 0x7500; // Mario can kick/stomp the shell when his Y position is greater than or equal to Y_MIN and less than Y_MAX
-const int Y_FLOOR = 0x6000; // Y position of the floor
+const int Y_MIN = 0x6100;
+const int Y_MAX = 0x8600; // Mario can kick/stomp the shell when his Y position is greater than or equal to Y_MIN and less than Y_MAX
+const int Y_FLOOR = 0x7000; // Y position of the floor
 
 /*
-Output format:
+Output format (outdated):
 
 If DO_JUMP is set to false:
 1st line: Starting Y subpixel (only the first hexadecimal digit)
@@ -34,45 +34,97 @@ If DO_JUMP is set to true:
 3rd and 4th lines: Same as 1st and 2nd lines, but for odd cointoss
 */
 
-// DO NOT CHANGE ANYTHING BELOW THIS LINE
-
 void advance(int& pos, int& speed, int& frame, bool jump) {
 	pos += speed;
 	speed += jump ? JUMP_GRAVITY : GRAVITY;
-	if (speed >= 0x480)
+	if ((speed >= 0x480 && speed < 0x500) || speed >= 0x580)
 		speed = 0x400;
 	++frame;
 }
 
 int jumpFrames = 0;
 
-int minFrames = 1000000000;
+double maxPPF = 0;
 int optJumpFrames = 0;
-vector<int> delays(TOTAL_KICKS, 0);
+int optFrame = 0;
+int optSac = 0;
+vector<int> frames(TOTAL_KICKS, 0);
+vector<int> curFrames(TOTAL_KICKS, 0);
+
+int cnt = 0;
+const vector<int> points = { 100, 200, 400, 500, 800, 1000, 2000, 4000, 5000 };
 
 //vector<int> seq = { 8, 6, 11, 9, 8, 10, 6, 7, 9, 6 };
 
-int solve(int pos, int speed, int frame, int kicks) {
+double solve(int pos, int speed, int frame, int kicks, int sac) {
 	if (kicks == TOTAL_KICKS) {
+		/*if (sac >= 800)
+			return 0;*/
+		//int i = 0;
+		/*if (sac > 0)
+			return 0;*/
+		//double res = (double)1 / (double)frame;
+		int frame0 = frame;
+		int a = 1000000000;
+		int p = frame % 2;
 		while (pos < Y_FLOOR || speed < 0) {
 			advance(pos, speed, frame, false);
+			/*++i;
+			if (i == 4 && (pos >= Y_MAX || pos < Y_MIN))
+				return 1000000001;*/
+			++a;
+			if (pos < Y_MIN && frame % 2 == p)
+				a = 0;
 			if (pos >= Y_FLOOR + 0x500 && speed >= 0)
-				return 1000000001;
+				return 0;
 		}
-		return frame;
+		//if (a > 5)
+			//return 0;
+		/*if (frame - frame0 > 13 || sac > 0)
+			return 0;
+		if (frame0 <= 127) {
+			cout << frame << " " << jumpFrames << " " << ((pos % 0x100) / 16) << endl;
+			for (int i = 0; i < TOTAL_KICKS; ++i)
+				cout << curFrames[i] << " ";
+			cout << endl;
+			++cnt;
+		}*/
+		double res = /*(double)(43300 - sac) / (double)((max(curFrames[TOTAL_KICKS - 1] - curFrames[0] + 18, frame + 1) + 1) / 2 * 2)*//*(double)1 / (double)((max(curFrames[TOTAL_KICKS - 1] - curFrames[0] + 18, frame + 1) + 1) / 2 * 2);*/(double)(43300 - sac) / (double)(frame);
+		/*if ((double)(43700 - sac) / (double)(frame + 5) > 313)
+			++cnt;
+		return (double)(43700 - sac) / (double)(frame + 5);*/
+		//double res = (double)(43300 - sac) / (double)((frame + 1) / 2 * 2);
+		if (res > 322) {
+			cout << frame << " " << sac << " " << (pos % 0x100) / 16 << endl;
+			for (int i = 0; i < TOTAL_KICKS; ++i)
+				cout << curFrames[i] << " ";
+			cout << endl;
+			++cnt;
+		}
+		if (res >= maxPPF) {
+			maxPPF = res;
+			optJumpFrames = jumpFrames;
+			optFrame = frame;
+			optSac = sac;
+		}
+		return res;
 	}
-	int totalRes = 1000000001;
+	double totalRes = 0;
 	vector<bool> canKickFrame(50);
+	int a = (kicks == 9 ? 1000000000 : 0);
 	for (int i = 0; ; ++i) {
 		canKickFrame[i] = (pos < Y_MAX && pos >= Y_MIN);
+		if (pos < Y_MIN)
+			a = i;
 		//cout << frame << " " << hex << pos << " " << speed << dec << endl;
-		if ((i >= 3 + KICK_TO_STOMP / 2 || (kicks == 0 && frame >= FIRST_KICK_FRAME + KICK_TO_STOMP)) && canKickFrame[i - KICK_TO_STOMP / 2] && canKickFrame[i] && speed >= 0x100) {
-			int res = solve(pos, -0x400 + speed % 0x100, frame, kicks + 1);
-			totalRes = min(totalRes, res);
-			if (res <= minFrames) {
-				minFrames = res;
+		if (((kicks != 0 && i >= 2 + KICK_TO_STOMP[kicks] / 2) || (kicks == 0 && frame >= FIRST_KICK_FRAME + KICK_TO_STOMP[kicks])) && canKickFrame[i - KICK_TO_STOMP[kicks] / 2] && canKickFrame[i] && speed >= 0x100 && (kicks != 0 || frame <= 7)/* && (kicks != 1 || frame == 22) && (kicks != 9 || i - KICK_TO_STOMP[kicks] / 2 >= a + 1) && (kicks != 1 || frame == 23) && (kicks != 9 || i == 7) && (kicks != 9 || pos < Y_FLOOR)*/) {
+			curFrames[kicks] = frame;
+			double res = solve(pos, -0x400 + speed % 0x100, frame, kicks + 1, sac + ((kicks != 0 && i == 2 + KICK_TO_STOMP[kicks] / 2) ? points[kicks - 1] : 0));
+			totalRes = max(totalRes, res);
+			if (res >= maxPPF) {
+				maxPPF = res;
 				optJumpFrames = jumpFrames;
-				delays[kicks] = i * 2;
+				frames[kicks] = frame;
 			}
 		}
 		advance(pos, speed, frame, frame < jumpFrames);
@@ -83,35 +135,59 @@ int solve(int pos, int speed, int frame, int kicks) {
 	return totalRes;
 }
 
-void output(int cointoss) {
-	cout << minFrames << " " << optJumpFrames << endl;
-	int s = cointoss;
-	for (int i = 0; i < TOTAL_KICKS; ++i) {
-		s += delays[i];
-		cout << (s - KICK_TO_STOMP) << " ";
-	}
+void output() {
+	cout << maxPPF << " " << optFrame << " " << optSac << " " << optJumpFrames << endl;
+	for (int i = 0; i < TOTAL_KICKS; ++i)
+		cout << frames[i] << " ";
 	cout << endl;
-	minFrames = 1000000000;
+	maxPPF = 0;
 }
 
 int main() {
 	if (DO_JUMP) {
 		for (jumpFrames = 1; jumpFrames <= MAX_JUMP_FRAMES; ++jumpFrames)
-			solve(Y_FLOOR, JUMP_INIT, 0, 0);
-		output(0);
+			solve(Y_FLOOR + 0x2000, JUMP_INIT, 0, 0, 0);
+		//output();
+		maxPPF = 0;
 		for (jumpFrames = 1; jumpFrames <= MAX_JUMP_FRAMES; ++jumpFrames)
-			solve(Y_FLOOR + JUMP_INIT, JUMP_INIT + JUMP_GRAVITY, 1, 0);
-		output(1);
+			solve(Y_FLOOR + 0x2000 + JUMP_INIT, JUMP_INIT + JUMP_GRAVITY, 1, 0, 0);
+		//output();
+		maxPPF = 0;
+		/*for (int comb = 0; comb < (1 << (TOTAL_KICKS - 1)); ++comb) {
+			for (int i = 1; i < TOTAL_KICKS; ++i)
+				KICK_TO_STOMP[i] = ((comb >> (i - 1)) % 2) ? 4 : 10;
+			for (jumpFrames = 1; jumpFrames <= MAX_JUMP_FRAMES; ++jumpFrames)
+				solve(Y_FLOOR, JUMP_INIT, 0, 0);
+			for (jumpFrames = 1; jumpFrames <= MAX_JUMP_FRAMES; ++jumpFrames)
+				solve(Y_FLOOR + JUMP_INIT, JUMP_INIT + JUMP_GRAVITY, 1, 0);
+		}*/
 	}
 	else {
 		for (int subpix = 0; subpix < 16; ++subpix) {
-			solve(Y_FLOOR + 16 * subpix, 0, 0, 0);
 			cout << subpix << endl;
-			output(0);
-			solve(Y_FLOOR + 16 * subpix, GRAVITY, 1, 0);
-			output(1);
+			solve(0x9000 + 16 * subpix, 0, 0, 0, 0);
+			//output();
+			maxPPF = 0;
+			solve(0x9000 + 16 * subpix, GRAVITY, 1, 0, 0);
+			//output();
+			maxPPF = 0;
 			cout << endl;
 		}
+		solve(0x6438, 0x400, 0, 0, 0);
+		//output();
+		solve(0x6838, 0x400, 1, 0, 0);
+		//output();
+		/*for (int comb = 0; comb < (1 << (TOTAL_KICKS - 1)); ++comb) {
+			for (int i = 1; i < TOTAL_KICKS; ++i)
+				KICK_TO_STOMP[i] = ((comb >> (i - 1)) % 2) ? 4 : 12;
+			for (int subpix = 0; subpix < 16; ++subpix) {
+				solve(0x9000 + 16 * subpix, 0, 0, 0);
+				minFrames = 1000000000;
+				solve(0x9000 + 16 * subpix, GRAVITY, 1, 0);
+				minFrames = 1000000000;
+			}
+		}*/
 	}
+	cout << cnt << endl;
 	return 0;
 }
